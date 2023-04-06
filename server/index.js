1e66cbaec67195248ad316cc975c9f9b9c63a0d7
Tosvng <altosintiams@gmail.com>
2023-04-06 09:25:54 -0500

    Added some protection to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 
@@ -10,9 +12,20 @@ function makeApp(database) {
   dotenv.config();
   const app = express();
   app.use(express.json());
+  app.use(compression());
+  app.use(helmet());
   app.use(cors());
   app.use(bodyParser.json({ limit: "30mb", extended: true }));
   app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+  //   // Set up rate limiter: maximum of twenty requests per minute
+  // var RateLimit = require("express-rate-limit");
+  // var limiter = RateLimit({
+  //   windowMs: 1 * 60 * 1000, // 1 minute
+  //   max: 20,
+  // });
+  // // Apply rate limiter to all requests
+  // app.use(limiter);
 
   //ROUTES
   app.use("/admin", adminRoutes);
