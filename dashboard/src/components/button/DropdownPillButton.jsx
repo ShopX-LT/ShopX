57c80bc11e94fbcc791d04d15e126e1c2efeb929
Tosvng <altosintiams@gmail.com>
2023-06-26 03:27:46 -0500

    Added sort functionality to order table

--- dashboard/src/components/button/DropdownPillButton.jsx
+++ dashboard/src/components/button/DropdownPillButton.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import { Button, Menu, MenuItem, styled } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import { updateOrder } from '../../services/OrderService';
+
 const OPTIONS = {
-  cancelled: { name: 'cancelled', color: 'error' },
-  pending: { name: 'pending', color: 'warning' },
+  cancelled: { name: 'cancelled', color: 'warning' },
+  pending: { name: 'pending', color: 'error' },
   delivered: { name: 'delivered', color: 'success' },
   ready: { name: 'ready', color: 'info' },
   inTransit: { name: 'in transit', color: 'primary' },
@@ -15,9 +18,24 @@ const StyledButton = styled(Button)(() => ({
   borderRadius: '50px',
 }));
 
-const PillDropdownButton = ({ status }) => {
+const PillDropdownButton = ({ order, status }) => {
+  const axiosPrivate = useAxiosPrivate();
+
   const [anchorEl, setAnchorEl] = useState(null);
   const [selected, setSelected] = useState(status || 'pending');
+
+  // FIX: useEffect should be moved higher up. If it is here the page will run the useEffect for the number of orders in the store because pillbutton will be created for each.
+  useEffect(() => {
+    const updateStatus = async () => {
+      const updatedOrder = { ...order, status: selected };
+      const response = await updateOrder(axiosPrivate, order.id, updatedOrder);
+      if (!response) {
+        return;
+      }
+      console.log(order);
+    };
+    updateStatus();
+  }, [selected]);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
