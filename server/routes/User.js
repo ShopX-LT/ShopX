53a4a97d1ecb55e1917d74a04d98e7d2fe122fb2
Tosvng <altosintiams@gmail.com>
2024-02-06 19:29:59 -0600

    fix production

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { handleStoreVisit, handleSubscribeToStore } = require('../controllers/user/userController');
+const { handleStoreVisits, handleSubscribeToStore } = require('../controllers/user/userController');
 
 const router = express.Router();
 
-router.post('/visit', handleStoreVisit);
+router.post('/visit', handleStoreVisits);
 router.get('/subscribe/:reference', handleSubscribeToStore);
 
 module.exports = router;
