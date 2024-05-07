af9da247ce5c3df161ad25c516761148a2ecb6b3
Tosvng <altosintiams@gmail.com>
2024-05-07 16:21:04 -0500

    remove essence

--- clients/essence/src/hooks/useStore.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import StoreContext from "../context/StoreProvider";
-
-const useStore = () => {
-  return useContext(StoreContext);
-};
-
-export default useStore;
