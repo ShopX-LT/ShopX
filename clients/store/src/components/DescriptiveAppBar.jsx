0685b72cff6f728028d6a9e0fba2a7efa9dc00cf
Tosvng <altosintiams@gmail.com>
2024-05-08 13:55:41 -0500

    refactor DescriptionPage

---
+++ clients/store/src/components/DescriptiveAppBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CloseIcon from '@mui/icons-material/Close';
+import { StyledDescriptionPageAppBar } from '../pages/styles';
+import { Button, IconButton, Toolbar, Typography } from '@mui/material';
+
+const DescriptiveAppBar = ({ design, handleClosePage, productTitle }) => {
+  return (
+    <StyledDescriptionPageAppBar design={design}>
+      <Toolbar>
+        <IconButton edge="start" color="inherit" onClick={handleClosePage} aria-label="close">
+          <CloseIcon />
+        </IconButton>
+        <Typography sx={{ ml: 2, flex: 1 }} variant="h6">
+          {productTitle}
+        </Typography>
+        <Button
+          autoFocus
+          color="inherit"
+          variant="contained"
+          onClick={handleClosePage}
+          sx={{
+            color: design.productContainer.productActionButtonTextColor,
+            background: design.productContainer.productActionButtonColor,
+          }}
+        >
+          {design.productContainer.productActionButtonText}
+        </Button>
+      </Toolbar>
+    </StyledDescriptionPageAppBar>
+  );
+};
+
+export default DescriptiveAppBar;
+
+DescriptiveAppBar.propTypes = {
+  design: PropTypes.object,
+  handleClosePage: PropTypes.func,
+  productTitle: PropTypes.string,
+};
--- clients/store/src/pages/DescriptionPage.jsx
+++ clients/store/src/pages/DescriptionPage.jsx
@@ -1,34 +1,14 @@
-import React, { forwardRef, useEffect, useState } from 'react';
-import {
-  AppBar,
-  Box,
-  Button,
-  CircularProgress,
-  Container,
-  Dialog,
-  IconButton,
-  Slide,
-  Toolbar,
-  Typography,
-} from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-cards';
-
-import './styles.css';
-
-// import required modules
-import { EffectCards } from 'swiper/modules';
+import PropTypes from 'prop-types';
+import { forwardRef } from 'react';
 import { useSelector } from 'react-redux';
+import { Box, CircularProgress, Container, Dialog, Slide, Typography } from '@mui/material';
+//
 import Review from '../lib/review/Review';
 import { fCurrency } from '../utils/formatNumber';
+import ImageSwiper from '../components/ImageSwiper';
 import useReview from '../sections/products/hooks/useReview';
+import DescriptiveAppBar from '../components/DescriptiveAppBar';
+import { FullViewPortBody, FlexColumnAlignedToStart, StyledProductPrice } from './styles';
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -41,108 +21,39 @@ const DescriptionPage = ({ isOpen, handleClose, product }) => {
   return (
     <div style={{ background: design.mainBackgroundColor }}>
       <Dialog fullScreen open={isOpen} onClose={handleClose} TransitionComponent={Transition}>
-        <AppBar
-          sx={{ position: 'relative', background: design.nav.navBackgroundColor, color: design.nav.navTextColor }}
-        >
-          <Toolbar>
-            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-              <CloseIcon />
-            </IconButton>
-            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              {product.title}
-            </Typography>
-            <Button
-              autoFocus
-              color="inherit"
-              variant="contained"
-              onClick={handleClose}
-              sx={{
-                color: design.productContainer.productActionButtonTextColor,
-                background: design.productContainer.productActionButtonColor,
-              }}
-            >
-              {design.productContainer.productActionButtonText}
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <Box
-          sx={{
-            backgroundColor: design.mainBackgroundColor,
-            color: design.hero.heroSubTextColor,
-            height: '100%',
-            maxWidth: '100vw',
-            overflowY: 'scroll',
-          }}
-        >
+        <DescriptiveAppBar design={design} productTitle={product.title} handleClosePage={handleClose} />
+        <FullViewPortBody design={design}>
           <Container>
-            <Box
-              sx={{
-                my: 4,
-                display: 'flex',
-                flexDirection: { xs: 'column', sm: 'row' },
-                justifyContent: 'Start',
-                alignItems: 'start',
-              }}
-            >
-              <Box sx={{ maxWidth: '99vw' }}>
-                <ImageSwiper product={product} />
-              </Box>
-              <Box>
-                <Typography variant="h3" fontWeight={500}>
-                  {product.title}
-                </Typography>
-                <Typography
-                  variant="h6"
-                  fontWeight={300}
-                  sx={{ textDecoration: product.discount ? 'line-through' : null }}
-                >
-                  {fCurrency(product.price)}
-                </Typography>
-                {product.discount ? (
-                  <Typography variant="h6" fontWeight={300}>
-                    {fCurrency(product.price * (1 - product.discount / 100))}
-                  </Typography>
-                ) : null}
-              </Box>
-            </Box>
-
+            <FlexColumnAlignedToStart>
+              <ImageSwiper productImages={product.imagesUrl} />
+              <ProductTitleAndPrice product={product} />
+            </FlexColumnAlignedToStart>
             <Box my={4}>
-              <Typography
-                variant="h3"
-                fontWeight={700}
-                marginBottom={1}
-                sx={{ borderBottom: 1, borderColor: 'divider' }}
-              >
+              <Typography variant="subtitle1" marginBottom={1} sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 Description
               </Typography>
-              <Typography variant="body1">{product.description}</Typography>
+              <Typography variant="body2">{product.description}</Typography>
               <Typography
-                variant="h3"
-                fontWeight={700}
-                marginTop={4}
+                variant="subtitle1"
+                marginTop={2}
                 marginBottom={1}
                 sx={{ borderBottom: 1, borderColor: 'divider' }}
               >
                 Reviews
               </Typography>
-              <Box>
-                {isLoading ? (
-                  <CircularProgress />
-                ) : (
-                  reviews.map((review, index) => (
-                    <Review
-                      key={index}
-                      select="default"
-                      name={review.name}
-                      rating={review.rating}
-                      comment={review.comment}
-                    />
-                  ))
-                )}
-              </Box>
+              {isLoading && <CircularProgress />}
+              {reviews.map((review, index) => (
+                <Review
+                  key={index}
+                  select="default"
+                  name={review.name}
+                  rating={review.rating}
+                  comment={review.comment}
+                />
+              ))}
             </Box>
           </Container>
-        </Box>
+        </FullViewPortBody>
       </Dialog>
     </div>
   );
@@ -150,40 +61,19 @@ const DescriptionPage = ({ isOpen, handleClose, product }) => {
 
 export default DescriptionPage;
 
-function ImageSwiper({ product }) {
+function ProductTitleAndPrice({ product }) {
+  const priceAfterDiscount = product.price * (1 - product.discount / 100);
   return (
-    <>
-      {/* <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className="mySwiper">
-        {product.imagesUrl.map((imageUrl, index) => {
-          return (
-            <SwiperSlide key={imageUrl}>
-              <img alt={'x'} src={imageUrl} />
-            </SwiperSlide>
-          );
-        })}
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-      </Swiper> */}
-      <Carousel
-        ariaLabel="product images"
-        showArrows={true}
-        showStatus={false}
-        showThumbs={true}
-        thumbWidth={100}
-        showIndicators={true}
-        infiniteLoop={true}
-        swipeable={true}
-        emulateTouch={true}
-        useKeyboardArrows={true}
-      >
-        {product.imagesUrl.map((imageUrl, index) => {
-          return (
-            <Box key={imageUrl}>
-              <img style={{ maxHeight: '520px', maxWidth: '420px', objectFit: 'cover' }} alt={'x'} src={imageUrl} />
-            </Box>
-          );
-        })}
-      </Carousel>
-    </>
+    <Box>
+      <Typography variant="h6">{product.title}</Typography>
+      <StyledProductPrice hasDiscount={product.discount}>{fCurrency(product.price)}</StyledProductPrice>
+      {product.discount ? <StyledProductPrice>{fCurrency(priceAfterDiscount)}</StyledProductPrice> : null}
+    </Box>
   );
 }
+
+DescriptionPage.propTypes = {
+  isOpen: PropTypes.bool,
+  handleClose: PropTypes.func,
+  product: PropTypes.object,
+};
